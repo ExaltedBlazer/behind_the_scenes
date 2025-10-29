992444d7ab29d32e32fd8c9fe7ea76f0efe2417f
Your Name <death5819@gmail.com>
2025-10-28 20:44:32 -0400

    completed logic for THIS keyword

--- script.js
+++ script.js
@@ -5,36 +5,94 @@
 //   return age;
 // };
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating new varible with the same name as outer scope's varible
-      const firstName = 'Steve';
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating new varible with the same name as outer scope's varible
+//       const firstName = 'Steve';
 
-      // Reassigning outer scope's variable
-      output = 'NEW OUTPUT!';
+//       // Reassigning outer scope's variable
+//       output = 'NEW OUTPUT!';
 
-      const str = `Oh and you're a millenial, ${firstName}`;
-      console.log(str);
+//       const str = `Oh and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    //add(2,3);
-    //console.log(millenial);
-    console.log(output);
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     //add(2,3);
+//     //console.log(millenial);
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Kaden';
+// calcAge(2004);
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Kaden';
+let job = 'teacher';
+const year = 1991;
+
+// Funtions
+//console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Kaden';
-calcAge(2004);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+// console.log(undefined);
+
+if (numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+const jonas = {
+  name: 'Jonas',
+  year: 1989,
+  calcAge: function () {
+    return 2037 - this.year;
+  },
+  lastName: 'Kai',
+  fullName: function () {
+    return this.name + ' ' + this.lastName;
+  },
+};
+
+let jonasAge = jonas.calcAge();
+const jonasName = jonas.fullName();
+console.log(`${jonasName} age is ${jonasAge}`);
